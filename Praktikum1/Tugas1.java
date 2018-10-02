bb2f663cabb707983de31381183c92c585e7519a
Muhammad Manan Shofa <36910674+mananshofa07@users.noreply.github.com>
2018-10-02 14:28:39 +0700

    Praktikum1

---
+++ Praktikum1/Tugas1.java
@@ -0,0 +1,15 @@
+public class Tugas1 {
+    public static void main(String[] args) {
+        System.out.println("\n\t\t\tProvinsi Jawa Tengah\n\t\t\tKabupaten Pekalongan\n");
+        System.out.println("\tNIK\t\t\t: 171530021");
+        System.out.println("\tNama\t\t\t: Muhammad Manan Shofa");
+        System.out.println("\tTempat/Tgl Lahir\t: Pekalongan, 7-April-1999");
+        System.out.println("\tJenis Kelamin\t\t: Laki-Laki");
+        System.out.println("\tAlamat\t\t\t: Bojong Minggir\n\t\tRT/RW\t\t: 013/007\n\t\tKel/Des\t\t: Bojong Minggir\n\t\tKecamatan\t: Bojong");
+        System.out.println("\tAgama\t\t\t: Islam");
+        System.out.println("\tStatus Perkawinan\t: Belum Kawin");
+        System.out.println("\tPekerjaan\t\t: PELAJAR/MAHASISWA");
+        System.out.println("\tKewarganegaraan\t\t: WNI");
+        System.out.println("\tBerlaku Hingga\t\t: Seumur Hidup");
+    }
+}
---
+++ Praktikum1/Tugas2.java
@@ -0,0 +1,12 @@
+public class Tugas2 {
+    public static void main(String[] args) { 
+        System.out.println("\n\t\t\tData Mahasiswa\n");
+        System.out.println("\tNIK\t\t\t: 171530021");
+        System.out.println("\tNama\t\t\t: Muhammad Manan Shofa");
+        System.out.println("\tTempat/Tgl Lahir\t: Pekalongan, 7-April-1999");
+        System.out.println("\tJenis Kelamin\t\t: Laki-Laki");
+        System.out.println("\tAlamat\t\t\t: Bojong Minggir\n\t\tRT/RW\t\t: 013/007\n\t\tKel/Des\t\t: Bojong Minggir\n\t\tKecamatan\t: Bojong");
+        System.out.println("\tAgama\t\t\t: Islam"); 
+        System.out.println("\tNo. Hp \t\t\t: 082323412526");
+    }
+}
